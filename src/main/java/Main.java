288b8967493bc2f717aee5379352e059118124f9
Rau <s2darauu@uni-trier.de>
2024-05-10 16:46:51 +0200

    Conflict wird erweitert

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,9 @@ public class Main {
         Factory factory = new Factory();
 
         Car car1 = new Car("red", "BMW", 5);
-        Car car2 = new Car("green", "Audi", 3);
-        Car car3 = new Car("blue", "VW", 3);
-
+        Car car2 = new Car("green", "Renaut", 3);
+        Car car3 = new Car("orange", "LADA", 3);
+	//Hier kommt ein Kommentar hin
         Car car4 = new Car("yellow", "Renault", 4);
 
         //An dieser Stelle befindet sich nun ein Array
