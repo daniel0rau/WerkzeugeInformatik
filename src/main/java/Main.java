60d2825f6f125baedf223806c89a044b24af8b65
Rau <s2darauu@uni-trier.de>
2024-05-10 16:44:50 +0200

    Hier wird gleich ein Konflikt ausgelöst

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,8 @@ public class Main {
 
         Car car1 = new Car("red", "BMW", 5);
         Car car2 = new Car("blue", "Audi", 3);
-        Car car3 = new Car("blue", "VW", 3);
-
+        Car car3 = new Car("Green", "Porsche", 3);
+//Ich ändere hier Files so wie ich will
         factory.buildCar(car1);
         factory.buildCar(car2);
     }
